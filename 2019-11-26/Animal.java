7c4fbd7cecbf786cc9eb6c4adb3e37a5f928df24
fengdecong <785608942@qq.com>
2019-11-26 22:31:10 +0800

    一个父类Animal

---
+++ 2019-11-26/Animal.java
@@ -0,0 +1,12 @@
+public class Animal {
+    int color;
+    int numberLegs;
+
+    Animal(){
+        this.color = color;
+        this.numberLegs = numberLegs;
+    }
+    public void scream(){
+        System.out.println("我是一个动物");
+    }
+}
