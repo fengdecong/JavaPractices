976182ca6bd09e26d0aed9df720dbdcef85d303b
fengdecong <785608942@qq.com>
2019-12-10 23:59:28 +0800

    异常处理

---
+++ 2019-12-10-2/P2019_12_10/CheckedExceptionDemo.java
@@ -0,0 +1,9 @@
+package P2019_12_10;
+
+public class CheckedExceptionDemo {
+    private static void method() throws RuntimeException {
+    }
+    public static void main(String[] args) {
+        method();
+    }
+}
---
+++ 2019-12-10-2/P2019_12_10/ThrowException.java
@@ -0,0 +1,8 @@
+package P2019_12_10;
+
+public class ThrowException {
+
+    public static void main(String[] args) {
+        throw new RuntimeException();
+    }
+}
