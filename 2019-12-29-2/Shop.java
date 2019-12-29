d8ea86e6f73f4c2e7d4120f443e4ffb8c539be24
fengdecong <785608942@qq.com>
2019-12-29 10:41:00 +0800

    Bean 设置商品属性

---
+++ 2019-12-29-2/Shop.java
@@ -0,0 +1,37 @@
+public class Shop {
+    private String sid;
+    private String name;
+    private int sprice;
+    private int snumber;
+    public String getSid() {
+        return sid;
+    }
+
+    public void setSid(String sid) {
+        this.sid = sid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getSprice() {
+        return sprice;
+    }
+
+    public void setSprice(int sprice) {
+        this.sprice = sprice;
+    }
+
+    public int getSnumber() {
+        return snumber;
+    }
+
+    public void setSnumber(int snumber) {
+        this.snumber = snumber;
+    }
+}
