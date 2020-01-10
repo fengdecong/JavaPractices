13d2951e2ed0f811aa1cea8786a4534fbdcd4be9
fengdecong <785608942@qq.com>
2020-01-10 20:18:55 +0800

    登陆失败返回的界面

---
+++ 2020-1-10/noLogin.java
@@ -0,0 +1,10 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+    <h1>异常登录！</h1>
+    <a href="welcome.jsp">返回登录界面</a>
+</body>
+</html>
