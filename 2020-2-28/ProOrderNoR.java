1f248b810f1963da65be036048acd7c8b40ab8cd
fengdecong <785608942@qq.com>
2020-02-28 17:14:08 +0800

    非递归前序遍历

--- 2020-2-28/ProOrderNoR.java
+++ 2020-2-28/ProOrderNoR.java
@@ -7,14 +7,15 @@ public static void ProOrderNoR(TreeNode root){
 		//走到空时结束
 		while(curNode != null){
 			//访问根节点
-			System.out.println(curNode.value);
+			System.out.println(curNode.value + " ");
 			//访问根的左子树
 			st.push(curNode);
-			curNode.left = curNode; 
+			curNode = curNode.left; 
 		}
 		//获取栈顶元素
 		curNode = st.pop();
 		//访问栈顶元素的右子树
 		curNode = curNode.right;
 	}
+	System.out.println();
 }
